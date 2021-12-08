daa46ff7f6fd47b93cfd74dc46c4a5f8b77301d9
naveenhpk <75996956+naveenhpk@users.noreply.github.com>
2021-12-08 15:55:35 +0530

    Add files via upload

---
+++ CO2/CO2_011.py
@@ -0,0 +1,8 @@
+import math
+tarea = lambda b,h : 1/2*b*h 
+rarea = lambda l,b : l*b 
+sarea = lambda a : a*a
+
+print("Area of Triangle :", tarea(20,60))
+print("Area of Rectangle:", rarea(10,30)) 
+print("Area of Square :", sarea(14)) 
---
+++ CO2/CO2_03.py
@@ -0,0 +1,3 @@
+list1 = [10, 20, 30, 40, 50]
+total = sum(list1)
+print("Sum of list : ",total)
